c39dbf124280d72280c6aa6de1317b3bc1d5cd53
suyatinglaura <e0492464@u.nus.edu>
2022-02-28 23:47:18 +0800

    about page

--- src/components/AboutMeridi.vue
+++ src/components/AboutMeridi.vue
@@ -1,28 +1,38 @@
 <template>
-  <el-row :gutter="20">
-    <el-col :span="12">
-      <div class="image">
-        <img id="logo" src="@/assets/TCM.jpg" alt="" />
-      </div>
-    </el-col>
+  <section class="hero">
+    <div class="hero-text container">
+      <section id="meridi">
+        <el-row :gutter="20">
+          <el-col :span="12">
+            <div class="image">
+              <img id="logo" src="@/assets/TCM.jpg" alt="" />
+            </div>
+          </el-col>
 
-    <el-col :span="12">
-      <div class="text">
-        <img id="quotation" src="@/assets/quotation-mark.png" alt="" />
-        <p id="slogan">TCM at your finger print</p>
-        <br />
-        <p id="description">
-          meridi serves to generate an alternative stream of revenue for
-          traditional TCM businesses by allowing prospective customers to
-          purchase their products and services online. This is targeted at
-          younger customers who are tech-savvy and highly value convenience when
-          making purchases. meridi also provides TCM businesses with an
-          opportunity to market their online presence, establishing their
-          position in the highly competitive market.
-        </p>
-      </div>
-    </el-col>
-  </el-row>
+          <el-col :span="12">
+            <div class="text">
+              <img
+                id="quotation"
+                src="@/assets/quotation-mark-black.png"
+                alt=""
+              />
+              <p id="slogan">TCM at your finger print</p>
+              <br />
+              <p id="description">
+                meridi serves to generate an alternative stream of revenue for
+                traditional TCM businesses by allowing prospective customers to
+                purchase their products and services online. This is targeted at
+                younger customers who are tech-savvy and highly value
+                convenience when making purchases. meridi also provides TCM
+                businesses with an opportunity to market their online presence,
+                establishing their position in the highly competitive market.
+              </p>
+            </div>
+          </el-col>
+        </el-row>
+      </section>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -31,7 +41,7 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .hero {
   background-attachment: fixed;
   position: relative;
@@ -47,6 +57,12 @@ export default {
 .el-row {
   margin-bottom: 20px;
 }
+#meridi {
+  position: absolute;
+  top: 300px;
+  left: 0px;
+  right: 0px;
+}
 #slogan {
   font-size: 40px;
 }
@@ -54,11 +70,13 @@ export default {
   font-size: 20px;
 }
 #logo {
-  width: 400px;
-  align-items: center;
+  width: 90%;
+}
+#text {
+  width: 90%;
 }
 #quotation {
-  width: 40px;
+  width: 10%;
   opacity: 0.5;
 }
 </style>
