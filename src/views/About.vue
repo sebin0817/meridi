2a534950c8e83c375eb13a43e44841ff95e752c5
sebin <e0552331@u.nus.edu>
2022-03-01 20:16:36 +0800

    AboutTCM Component

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,144 +1,23 @@
 <template>
   <div class="About">
-    <section class="hero">
-      <div class="hero-text container">
-        <h4>header4</h4>
-        <hr />
-        <h2>
-          Webpage <br />
-          Title
-        </h2>
-        <hr />
-      </div>
-    </section>
-    <section>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum
-        corporis, expedita odit aliquam ad aut ullam. Molestiae laborum magnam
-        aut blanditiis iste ipsa, doloremque necessitatibus? Exercitationem
-        rerum temporibus consectetur voluptatum minus quos, ab quisquam sequi
-        obcaecati reiciendis! Vero blanditiis totam explicabo, corporis
-        consequatur nobis maxime tempore ipsum adipisci natus, saepe iste sunt
-        excepturi eius cum eaque dicta nostrum recusandae labore accusamus?
-        Similique cum veritatis a vitae ea porro possimus hic sit minima
-        repellendus, rerum, accusantium, laborum excepturi? Quasi nam modi
-        aliquam omnis quibusdam ipsam eius eos officiis. Aperiam, animi, vero
-        itaque autem eum est similique hic porro quas modi, deserunt fugit
-        voluptas saepe consequatur? Officiis sapiente totam excepturi qui,
-        dolore unde cum et ratione asperiores harum placeat eum laboriosam quia
-        fuga adipisci doloremque. Dolorum dolorem obcaecati enim amet eos
-        facilis illo inventore tenetur. Obcaecati nesciunt excepturi, est eos
-        laudantium voluptas ex reiciendis, iure expedita aliquam corporis ut,
-        tempora non sint ipsam accusamus velit soluta commodi quos repellendus
-        deserunt beatae? Ipsum sequi quis quaerat distinctio vitae amet,
-        molestiae eius nisi laborum nulla officia exercitationem fugiat at
-        consectetur illo eveniet, impedit excepturi nam aperiam id reiciendis?
-        Temporibus fuga placeat asperiores necessitatibus labore iusto fugit
-        laboriosam itaque earum aperiam et adipisci, totam numquam nisi magni.
-        Quasi sed earum cupiditate, iure voluptates consequuntur quibusdam iusto
-        itaque, veniam ad, repellendus quod odit laudantium dolorum velit atque
-        totam. Quasi, labore. Sint eius praesentium sapiente natus
-        exercitationem magni mollitia sed cumque esse? Perspiciatis unde fuga
-        error reiciendis omnis qui doloremque, adipisci ex, eveniet eos
-        temporibus, a iure sit repellat quaerat velit voluptates eius. Sit,
-        nobis aliquid magni nesciunt, ea omnis fuga reiciendis illo nostrum iure
-        temporibus molestias architecto dicta amet id. Nisi quaerat inventore
-        perferendis iure doloremque rem necessitatibus eligendi dolorum quam
-        aliquam provident nihil incidunt, minus aperiam hic ullam aliquid.
-        Repudiandae quam, saepe suscipit blanditiis tempore provident quidem
-        fugiat beatae culpa. Molestias earum ab minima architecto vitae suscipit
-        eaque iure qui, velit recusandae saepe placeat nihil voluptatum culpa
-        modi nulla enim? Eveniet illo quisquam non ratione, expedita
-        exercitationem ad dolorum error sapiente, quod perferendis quibusdam,
-        eos recusandae porro molestiae neque minima similique corporis omnis
-        ullam odit. Numquam reprehenderit sint libero, pariatur ducimus dicta
-        illo repudiandae? Quidem perferendis magnam deleniti sequi animi impedit
-        assumenda magni, delectus quas sapiente, aspernatur molestias possimus
-        dolor. Dolores minus libero voluptatem quas debitis ipsam possimus vero
-        omnis nulla assumenda neque repellat enim natus, sed odio unde obcaecati
-        dolore. Molestias quibusdam, expedita error asperiores deleniti dolor
-        natus porro non vel perspiciatis mollitia delectus officia veritatis
-        possimus consequatur. Aliquam totam mollitia sequi? Iure reiciendis nam,
-        sequi commodi blanditiis atque tenetur amet aliquam velit molestiae in
-        repudiandae id vitae optio minus quas numquam esse ex facere officiis
-        veritatis aliquid! Voluptatem eos officia vitae illum quam, natus animi
-        consequuntur perferendis, dignissimos nisi iusto ipsa veniam cumque
-        aspernatur delectus molestias ratione ab id eligendi modi. Repellendus
-        incidunt vel cumque totam, alias unde consectetur iusto aliquid
-        assumenda expedita accusamus odit voluptate sit quod. Nesciunt cumque
-        voluptatibus in. Assumenda ratione vel velit. Tempore repellendus iste,
-        corporis illo sint quisquam facilis odio, aspernatur dolor vitae non
-        eius consectetur veniam ab vel voluptatibus temporibus soluta eaque
-        ratione deleniti nihil commodi voluptas assumenda? Delectus cumque, quos
-        doloremque laboriosam libero adipisci error molestias nobis fugiat quae
-        rerum tenetur sunt. Dolore, aspernatur. Sunt quaerat rem qui ipsam
-        distinctio repudiandae doloremque amet quia, odio at reprehenderit quod
-        praesentium iste rerum esse cupiditate excepturi et maiores similique
-        molestiae quam, ducimus deserunt maxime. Architecto autem enim quis.
-        Voluptas veniam repudiandae ratione praesentium eos sequi dolorum
-        veritatis molestias, soluta magni est, nostrum temporibus natus dicta
-        ad. Molestiae blanditiis recusandae sed nisi nesciunt sapiente
-        voluptatem perferendis numquam! Blanditiis molestias dolores provident
-        eum quis quos harum quidem! Corrupti assumenda iusto eveniet tempore
-        molestiae tenetur minima architecto repellat praesentium vel? Inventore
-        corporis dolores eum, vero autem earum minima cum similique corrupti quo
-        recusandae blanditiis perferendis et iste repudiandae maxime aut alias
-        dolore in excepturi ducimus temporibus quia expedita iure. Dolore
-        distinctio dignissimos natus dicta accusantium error reprehenderit
-        neque, tempora labore a ducimus unde impedit aut beatae, ipsum, dolores
-        tempore nemo rerum sint! Repellendus nam et nemo nesciunt debitis
-        consectetur quibusdam animi atque neque, ex eligendi laboriosam totam
-        cumque saepe doloremque quia sit tenetur alias, soluta tempora. Dolorum
-        rerum ex quae consequuntur, ipsum tempore atque, excepturi, ullam sequi
-        fuga earum rem accusantium! Deleniti, est repellendus provident, non
-        aspernatur cupiditate iure, odio alias id unde exercitationem sed soluta
-        minus? Est nulla vitae ad quisquam quae, tempora nihil incidunt quod
-        unde aspernatur dolor. Labore voluptas ratione debitis, aliquid tempora
-        molestias maiores numquam doloremque expedita perferendis repellat
-        quidem, ipsam veritatis delectus quod aspernatur qui itaque nihil vero
-        quasi eos illo. Minima autem porro, voluptatibus dignissimos, nemo id
-        asperiores at culpa nobis omnis hic. Quam voluptatum laudantium fugit
-        eius aut sint modi dignissimos commodi quibusdam cum illum veniam
-        maxime, dolor porro optio nobis libero, itaque a, veritatis blanditiis
-        quisquam reprehenderit eaque! Aliquid similique quia consequatur, sunt
-        alias, explicabo beatae quo modi expedita, et facilis illum officia in
-        numquam aut optio quae laboriosam a mollitia tenetur unde? Esse aperiam
-        doloremque ab, quidem officia placeat rem dolor illum autem mollitia, a
-        tempore iure temporibus recusandae maxime, nam consectetur quod ullam
-        similique reprehenderit. Amet, deserunt eligendi minus maiores deleniti
-        eaque vitae, asperiores dignissimos natus nisi blanditiis a magnam enim
-        ipsa. Voluptate nobis dolor obcaecati sequi necessitatibus ea, debitis
-        voluptas molestias, modi in sit ipsam tempora aliquid inventore. Quae
-        voluptatem veniam nesciunt illo, reprehenderit earum animi sed expedita,
-        ad totam magni fuga possimus iure officia sapiente et unde quasi ipsa.
-        Iusto assumenda voluptas non. Iure autem adipisci eos doloribus
-        perspiciatis id aut repellat perferendis! Possimus nostrum, quia
-        distinctio amet harum excepturi in nam, est fuga asperiores delectus!
-        Praesentium id quod officia? Deleniti vero dolore mollitia consequatur
-        illo? Dolore magni accusantium perferendis assumenda at quaerat odio in
-        voluptatibus iusto sapiente nesciunt nam reiciendis odit ad, harum
-        consequatur quos repellendus omnis velit? Iure molestiae veritatis,
-        culpa ratione labore asperiores vitae sunt enim ipsam amet impedit
-        accusantium modi similique pariatur odio suscipit nemo, fugit optio
-        distinctio, officia voluptatibus! Mollitia distinctio eius quaerat
-        repudiandae?
-      </p>
-    </section>
+    <AboutTCM />
+    <!-- <section class="hero"></section> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import AboutTCM from "/src/components/AboutTCM";
 
 export default {
   name: "About",
-  components: {},
+  components: { AboutTCM },
 };
 </script>
 
 <style lang="scss" scoped>
 .hero {
-  background-image: url("../assets/hero-bg.png");
+  // background-image: url("../assets/hero-bg.png");
   background-attachment: fixed;
   position: relative;
   height: 100vh;
--- src/views/Clinics.vue
+++ src/views/Clinics.vue
@@ -1,13 +1,88 @@
 <template>
   <section class="hero">
     <div class="hero-text container">
-      <h4>Clinics Page</h4>
+      <!-- <h4>Cart Page</h4> -->
+      <el-form
+        :inline="false"
+        :model="formInline"
+        class="demo-form-inline"
+        :size="formSize"
+      >
+        <el-form-item>
+          <el-select
+            v-model="formInline.region"
+            multiple
+            placeholder="Filter by Service(s)"
+          >
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            >
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-row :gutter="5">
+            <el-col :span="15">
+              <el-input
+                v-model="formInline.user"
+                placeholder="Postal Code"
+                style="width: 100%"
+              ></el-input>
+            </el-col>
+            <el-col :span="2">
+              <el-button
+                type="warning"
+                :icon="Search"
+                @click="onSubmit"
+              ></el-button>
+            </el-col>
+          </el-row>
+        </el-form-item>
+      </el-form>
     </div>
   </section>
 </template>
 
-<script>
-export default {};
+<script setup>
+import { reactive, ref } from "vue";
+import { Search } from "@element-plus/icons-vue";
+
+const formSize = ref("");
+const formInline = reactive({
+  input: "",
+});
+const options = [
+  {
+    value: "Accupuncture",
+    label: "Accupuncture",
+  },
+  {
+    value: "Tunia",
+    label: "Tunia",
+  },
+  {
+    value: "Herbal Medication",
+    label: "Herbal Medication",
+  },
+  {
+    value: "Gua Sha",
+    label: "Gua Sha",
+  },
+  {
+    value: "Cupping",
+    label: "Cupping",
+  },
+  {
+    value: "Device Therapy",
+    label: "Device Therapy",
+  },
+];
+const onSubmit = () => {
+  console.log("submitted!");
+};
 </script>
 
 <style scoped>
