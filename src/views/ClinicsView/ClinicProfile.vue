832e6ef86b1bd7b238ba70bafc9b493cf15e1098
downmeanhigh <daominhhai@u.nus.edu>
2022-03-24 14:55:16 +0800

    update ClinicProfile

--- src/views/ClinicsView/ClinicProfile.vue
+++ src/views/ClinicsView/ClinicProfile.vue
@@ -1,40 +1,29 @@
 <template>
   <section class="hero">
     <div class="hero-text container">
-<<<<<<< HEAD
       <ClinicInfo :useremail="useremail"/>
-=======
       <EditParticularsForm/>
->>>>>>> hai
     </div>
   </section>
 </template>
 
 <script>
-<<<<<<< HEAD
 import ClinicInfo from '../../components/ClinicsProfilePage/ClinicInfo.vue'
+import EditParticularsForm from '@/components/ClinicsProfilePage/EditParticularsForm.vue'
 
 
 export default {
   name: 'ClinicProfile',
   components: {
     ClinicInfo,
+    EditParticularsForm
   }, 
   computed: {
     useremail() {
       return sessionStorage.getItem("useremail");
     }
   }
-=======
-import EditParticularsForm from '../../components/ClinicsProfilePage/EditParticularsForm.vue'
-
-export default {
-    name: 'EditCustomerParticulars',
-    components: {
-      EditParticularsForm,
-    },
->>>>>>> hai
-};
+}
 </script>
 
 <style scoped>
@@ -46,11 +35,7 @@ export default {
 .hero-text {
   height: 100%;
   display: flex;
-<<<<<<< HEAD
-  flex-direction: column; 
-=======
   flex-direction: column;
->>>>>>> hai
   justify-content: center;
   color: black;
 }
